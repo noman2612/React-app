868c9d038e86d4f206d577754659ac26256f5c31
NomanHossain <nomanhossaim8743@gmail.com>
2023-04-05 01:13:21 +0600

    new add

--- src/COMPONENT/ODERS/Oders.jsx
+++ src/COMPONENT/ODERS/Oders.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviowItem from '../reviowItem/ReviowItem';
 import'./oder.css'
+import { removeFromDb } from '../../utilities/fakedb';
 const Oders = () => {
-    const cart = useLoaderData()
+    const CartProduct = useLoaderData()
+    const [cart,setCart] = useState(CartProduct)
+    const handelCartProduct =(id)=>{
+    
+        const DeleadProduct = cart.filter(pd=>pd.id !==id)
+        setCart(DeleadProduct)
+          removeFromDb(id)
+
+    }
     return (
         <div className='shping-container'>
-           <div className=''>
+           <div className='reviow-containet'>
            {cart.map(product=><ReviowItem 
            key={product.id}
            product={product}
+           handelCartProduct={handelCartProduct}
            ></ReviowItem>)}
            </div>
            <div className='cart-container'>
--- src/COMPONENT/reviowItem/ReviowItem.jsx
+++ src/COMPONENT/reviowItem/ReviowItem.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
 import'./reviow.css'
-const ReviowItem = ({product}) => {
+const ReviowItem = ({product,handelCartProduct}) => {
+    console.log(product);
+    const {name,img,quantity,price,id} = product
     return (
         <div className='revowItem'>
- <h1>hello</h1>
+          <img src={img} alt="" />
+          <div className="review-info">
+          <p className='name-product'>  {name}</p>
+          <p>price:<samp className='text-org'> ${price}</samp></p>
+          <p> order  quantity<samp className='text-org'> {quantity}</samp></p>
+         
+          </div>
+       <button onClick={()=>handelCartProduct(id)} className='btn-delete'> <FontAwesomeIcon className='delead-icon' icon={faTrash} />
+</button>
         </div>
     );
 };
